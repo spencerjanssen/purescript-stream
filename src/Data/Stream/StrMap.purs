34f66791aaa9d4afda0f65930fb24854ba77f35b
Spencer Janssen <spencerjanssen@gmail.com>
2016-06-07 01:08:33 -0500

    Use eachEff instead of hand coded JS in StrMap

--- src/Data/Stream/StrMap.purs
+++ src/Data/Stream/StrMap.purs
@@ -2,21 +2,20 @@ module Data.Stream.StrMap (stream, unstream) where
 
 import Data.Stream
 import Data.Stream.Array as Arr
-import Control.Monad.Eff (Eff, runPure)
+import Control.Monad.Eff (runPure)
 import Data.StrMap (runST, keys, StrMap)
 import Data.StrMap.ST (poke, new)
 import Data.StrMap.Unsafe (unsafeIndex)
+import Data.Stream.Operations (eachEff)
 import Data.Tuple (Tuple(Tuple))
-import Prelude (void, Unit, pure, ($), map, bind)
+import Prelude (void, pure, ($), map, bind)
 
 stream :: forall a. StrMap a -> Stream (Tuple String a)
 stream arr = map (\i -> Tuple i $ unsafeIndex arr i) $ Arr.stream $ keys arr
 
 unstream :: forall a. Stream (Tuple String a) -> StrMap a
-unstream (Stream comb) = comb \next s0 -> runPure (runST (do
+unstream s = runPure (runST (do
     res <- new
-    playeff next s0 (\(Tuple k v) -> void $ poke res k v)
+    eachEff (\(Tuple k v) -> void $ poke res k v) s
     pure res
     ))
-
-foreign import playeff :: forall s a eff. (s -> Step s a) -> s -> (a -> Eff eff Unit) -> Eff eff Unit
