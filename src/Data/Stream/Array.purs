080ee6cd02917b681425779571c6bffdf763948d
Spencer Janssen <spencerjanssen@gmail.com>
2016-06-07 01:14:43 -0500

    More efficient Array.unstream

--- src/Data/Stream/Array.purs
+++ src/Data/Stream/Array.purs
@@ -2,17 +2,18 @@ module Data.Stream.Array where
 
 import Data.Stream
 import Data.Array as Array
+import Control.Monad.Eff (runPure)
+import Data.Array.ST (pushSTArray, emptySTArray, runSTArray)
 import Data.Array.Unsafe (unsafeIndex)
-import Prelude ((+), (>=), (<>))
+import Data.Stream.Operations (eachEff)
+import Prelude (void, (+), (>=), (<<<), bind, pure)
 
--- todo make this efficient
 unstream :: forall a. Stream a -> Array a
-unstream (Stream comb) = comb \next s0 ->
-    let go s arr = case next s of
-                    Done -> arr
-                    Yield x s' -> go s' (arr <> [x])
-                    Skip s' -> go s' arr
-    in go s0 []
+unstream s = runPure (runSTArray do
+    res <- emptySTArray
+    eachEff (void <<< pushSTArray res) s
+    pure res
+    )
 
 stream :: forall a. Array a -> Stream a
 stream arr = Stream \comb -> comb next 0
